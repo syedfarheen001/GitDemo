96a8399479d544e273174196d3fc58dd3ef9f149
syedfarheen001 <syedfarheen001@gmail.com>
2022-12-23 21:58:26 +0530

    changed code

---
+++ src/test/java/rahulShettyAcademy/ExtendReports/newFile.java
@@ -0,0 +1,12 @@
+package rahulShettyAcademy.ExtendReports;
+
+public class newFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello");
+		
+	}
+
+}

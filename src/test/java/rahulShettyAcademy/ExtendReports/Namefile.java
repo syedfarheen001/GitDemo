c6747722ea09203d4cc1479951188426e07d3e86
syedfarheen001 <syedfarheen001@gmail.com>
2022-12-24 21:27:16 +0530

    Architect work

---
+++ src/test/java/rahulShettyAcademy/ExtendReports/Namefile.java
@@ -0,0 +1,12 @@
+package rahulShettyAcademy.ExtendReports;
+
+public class Namefile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Thank You");
+		
+	}
+
+}
--- src/test/java/rahulShettyAcademy/ExtendReports/newFile.java
+++ src/test/java/rahulShettyAcademy/ExtendReports/newFile.java
@@ -6,7 +6,17 @@ public class newFile {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello");
+	
 		
 	}
-
+	
+	public void add()   {
+		
+		int a=2;
+		int b=3;
+		int c=a+b;
+		System.out.println(c);
+		
+	}
+  
 }
